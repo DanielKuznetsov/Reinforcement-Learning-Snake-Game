8d2bee21291e98cb1b55deccfadbc63290b5aa7d
Daniel Kuznetsov <danilgkuznet@icloud.com>
2023-02-05 20:28:10 -0800

    Initialize the snake's and food's positions

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 import random
 
 from enum import Enum
+from collections import namedtuple
 
 pygame.init()
 
@@ -10,6 +11,9 @@ class Direction(Enum):
     LEFT = 2
     UP = 3
     DOWN = 4
+
+Point = namedtuple('Point', 'x', 'y')
+BLOCK_SIZE = 20
 
 class SnakeGame:
 
@@ -24,6 +28,23 @@ class SnakeGame:
 
         # Initialize the game state (direction, food placement)
         self.direction = Direction.RIGHT
+
+        # Initialize snake
+        self.head = Point(self.width / 2, self.height / 2)
+        self.snake = [self.head,
+                      Point(self.head.x - BLOCK_SIZE, self.head.y),
+                      Point(self.head.x - (2 * BLOCK_SIZE), self.head.y)]
+        self.score = 0
+        self.food = None
+        self._place_food()
+
+    def _place_food(self):
+        x = random.randint(0, (self.w - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        y = random.randint(0, (self.h - BLOCK_SIZE) // BLOCK_SIZE) * BLOCK_SIZE
+        self.food = Point(x, y)
+
+        if self.food in self.snake:
+            self._place_food()
 
     def play_step(self):
         pass
