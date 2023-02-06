bdbb42dc089f703a9f78800e1719e9a317523acf
Daniel Kuznetsov <danilgkuznet@icloud.com>
2023-02-05 20:05:22 -0800

    Initialize pygame and create SnakeGame class (include default values)

---
+++ game.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+
+pygame.init()
+
+class SnakeGame:
+
+    def __init__(self, width=640, height=480):
+        self.width = width
+        self.height = height
+
+        # Initialize the display
+
+        # Initialize the game state
+
+    def play_step(self):
+        pass
+
+
+if __name__ == '__main__':
+    game = SnakeGame()
+
+    # Game loop
+    while True:
+        game.play_step()
+
+        # Break the loop if game is over
+
+
+    pygame.quit()
